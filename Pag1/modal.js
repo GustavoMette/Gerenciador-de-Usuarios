74bd14bb98ec5733e3f5abeffda162eb9f3886e2
Rodrigo Valim <116669651+RodrigoZV@users.noreply.github.com>
2023-03-28 16:42:34 -0300

    java

---
+++ Pag1/modal.js
@@ -0,0 +1,16 @@
+let modal = document.getElementById("myModal");
+let span = document.getElementsByClassName("close")[0];
+
+function showModal() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
---
+++ Pag2/login.js
@@ -0,0 +1,14 @@
+
+        function logar(){
+
+            var login = document.getElementById('login').value;
+            var senha = document.getElementById('senha').value;
+
+            if(login == "admin" && senha == "admin"){
+                alert('Sucesso');
+                location.href = "home.html";
+            }else{
+                alert('Usuario ou senha incorretos');
+            }
+
+        }
