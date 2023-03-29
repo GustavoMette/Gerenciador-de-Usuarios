ad5b679e027b62a2f83c7cc766fcfc283c054fa6
Rodrigo Valim <116669651+RodrigoZV@users.noreply.github.com>
2023-03-29 16:35:29 -0300

    update

--- Pag2/login.js
+++ Pag2/login.js
@@ -1,7 +1,7 @@
 
         function logar(){
 
-            var login = document.getElementById('login').value;
+            login = document.getElementById('login').value;
             var senha = document.getElementById('senha').value;
 
             if(login == "admin" && senha == "admin"){
