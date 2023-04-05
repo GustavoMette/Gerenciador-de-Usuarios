00df213333593367e4943e5a91df84569f1599c9
Rodrigo Valim <116669651+RodrigoZV@users.noreply.github.com>
2023-04-05 16:42:58 -0300

    .

--- Pag1/menu.js
+++ Pag1/menu.js
@@ -1,19 +1,102 @@
+let users = [];
 
-const nome = document.querySelector('#nome')
-const email = document.querySelector('#email')
-const senha = document.querySelector('#senha')
-const idade = document.querySelector('#idade')
-const alçadas = document.querySelector('#alçadas')
-const btnSalvar = document.querySelector('#btnSalvar')
+function addUser(name, email, password, age, position) {
+    let user = {
+      id: users.length + 1,
+      name: name,
+      email: email,
+      password: password,
+      age: age,
+      position: position
+    };
+    users.push(user);
+  }
 
-const button = document.querySelector('#button')
-const modal = document.querySelector('#dialog')
-const modal2 = document.querySelector('.modal-teste')
-const buttonClose = document.querySelector('#dialogbutton')
+  function findUserById(id) {
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].id === id) {
+        return users[i];
+      }
+    }
+    return null;
+  }
 
- function openModal () {
+  function editUser(id, name, email, password, age, position) {
+    let user = findUserById(id);
+    if (user) {
+      user.name = name;
+      user.email = email;
+      user.password = password;
+      user.age = age;
+      user.position = position;
+      return true;
+    }
+    return false;
+  }
 
-  modal2.show();
+  function deleteUser(id) {
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].id === id) {
+        users.splice(i, 1);
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  function addUser() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let age = document.getElementById("age").value;
+    let position = document.getElementById("position").value;
 
- }
+    addUser(name, email, password, age, position);
+renderUserList();
 
+  }
+
+  function editUser(id) {
+    let name = prompt("Enter new name:");
+    let email = prompt("Enter new email:");
+    let password = prompt("Enter new password:");
+    let age = prompt("Enter new age:");
+    let position = prompt("Enter new position:");
+
+    editUser(id, name, email, password, age, position);
+renderUserList();
+  }
+
+  function deleteUser(id) {
+    deleteUser(id);
+    renderUserList();
+    }
+    
+    function renderUserList() {
+    let userList = document.getElementById("user-list");
+    userList.innerHTML = "";
+
+    for (let i = 0; i < users.length; i++) {
+        let user = users[i];
+        let li = document.createElement("li");
+        li.textContent = user.name;
+        
+        let editButton = document.createElement("button");
+        editButton.textContent = "Edit";
+        editButton.onclick = function() {
+          editUser(user.id);
+        };
+        
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.onclick = function() {
+          deleteUser(user.id);
+        };
+        
+        li.appendChild(editButton);
+        li.appendChild(deleteButton);
+        userList.appendChild(li);
+      }
+    }
+
+    renderUserList();
