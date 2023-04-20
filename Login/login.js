3bf299855d7ad87b35b3245c890ee50f8fc0540f
João Pedro Rostovcev Krause <91441627+JoaoRostovcev@users.noreply.github.com>
2023-04-20 13:05:07 -0300

    commit

--- Login/login.js
+++ Login/login.js
@@ -1,14 +1,34 @@
+console.log(123)
+let usuarioCadastrado = {
+    usuarios:[]
+}
 
-        function logar(){
 
-            login = document.getElementById('login').value;
-            var senha = document.getElementById('senha').value;
+let FILTRO = ''
 
-            if(login == "admin" && senha == "admin"){
-                alert('Sucesso');
-                location.href = "home.html";
-            }else{
-                alert('Usuario ou senha incorretos');
-            }
 
-        }
+function graletBD(){
+    localStorage.setItem(KEY_BD, JSON.stringify(usuarioCadastrado) )
+}
+
+function lerBD(){
+    const data = localStorage.getItem(KEY_BD)
+    if(data){
+        listaRegistros = JSON.parse(data)
+    }
+}
+
+function logar(){
+
+    var login = document.getElementById(data.login).value;
+    var senha = document.getElementById(data.senha).value;
+
+    if(login == data.login && senha == data.senha){
+        alert('Sucesso');
+      window.location.href = "home.html";
+    }else{
+        alert('Usuario ou senha incorretos');
+    }
+    
+
+}
